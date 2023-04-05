e36319f5859a4cbe3a8f5a5b39999594c899835a
All1in <bm3023068@gmail.com>
2023-04-05 15:49:55 +0300

    key component

--- src/components/KeyBoard/KeyBoard.jsx
+++ src/components/KeyBoard/KeyBoard.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { keys1, keys2, keys3 } from "../../keys";
+import Key from "../Key/Key";
 
 const KeyBoard = () => {
     return (
-        <div>
-            KeyBoard
+        <div className="keyboard">
+            <div className="line1">
+                {keys1.map((key) => {
+                    return <Key keyVal={key} />;
+                })}
+            </div>
+            <div className="line2">
+                {keys2.map((key) => {
+                    return <Key keyVal={key} />;
+                })}
+            </div>
+            <div className="line3">
+                <Key keyVal={"ENTER"} bigKey />
+                {keys3.map((key) => {
+                    return <Key keyVal={key} />;
+                })}
+                <Key keyVal={"DELETE"} bigKey />
+            </div>
         </div>
     );
 };
